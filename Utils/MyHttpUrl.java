f48e5e64332efa1a88e6bf560b42134c24d9d571
wll <2306741930@qq.com>
2018-05-10 13:49:30 +0800

    Initial commit

---
+++ Utils/MyHttpUrl.java
@@ -0,0 +1,4 @@
+package com.example.rikao_day02.Utils;
+
+public class MyHttpUrl {
+}
---
+++ Utils/MyImageLoder.java
@@ -0,0 +1,7 @@
+package com.example.rikao_day02.Utils;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+
+public class MyImageLoder {
+
+}
